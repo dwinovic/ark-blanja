190816293390976425190be6b30cab0ba7691347
dwinovic <nd.cahya19@gmail.com>
2021-06-29 20:17:49 +0700

    edit host

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,6 +1,19 @@
 const goHome = () => {
-  let url = 'http://127.0.0.1:5500/src/pages/homepage.html'
-  console.log(url);
-  window.location = url
+  let urlHomeLocal = 'http://127.0.0.1:5500/src/pages/homepage.html';
+  let HOST = 'https://dreamy-haibt-870680.netlify.app/';
+  let urlHomeHOST = 'https://dreamy-haibt-870680.netlify.app/src/pages/homepage.html';
+
+
+
+  let checkProduction = window.document.location.href;
+  checkProduction;
+
+  if (checkProduction === HOST) {
+    console.log(urlHomeHOST);
+    return window.location = urlHomeHOST;
+  }
+
+  console.log(urlHomeLocal);
+  window.location = urlHomeLocal
 
 };
