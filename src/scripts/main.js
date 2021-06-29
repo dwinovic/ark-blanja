dcf1fc27f8ac0d04d8248b5e3f3c0bfbdd4e7315
dwinovic <nd.cahya19@gmail.com>
2021-06-29 20:02:39 +0700

    fixing link signup and login

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,5 +1,6 @@
-const homepage = 'newPage.html';
-
 const goHome = () => {
-  return (location.href = 'src/pages/homepage.html');
+  let url = 'http://127.0.0.1:5500/src/pages/homepage.html'
+  console.log(url);
+  window.location = url
+
 };
