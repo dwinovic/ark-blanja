ad5b8c8c0326aa443ba92592a5ca0ad847e380b9
dwinovic <nd.cahya19@gmail.com>
2021-06-30 21:49:05 +0700

    final app v1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,31 @@
 const goHome = () => {
   let urlHomeLocal = 'http://127.0.0.1:5500/src/pages/homepage.html';
   let HOST = 'https://dreamy-haibt-870680.netlify.app/';
-  let urlHomeHOST = 'https://dreamy-haibt-870680.netlify.app/src/pages/homepage.html';
-
-
+  let urlHomeHOST =
+    'https://dreamy-haibt-870680.netlify.app/src/pages/homepage.html';
 
   let checkProduction = window.document.location.href;
   checkProduction;
 
   if (checkProduction === HOST) {
     console.log(urlHomeHOST);
-    return window.location = urlHomeHOST;
+    return (window.location = urlHomeHOST);
   }
 
   console.log(urlHomeLocal);
-  window.location = urlHomeLocal
+  window.location = urlHomeLocal;
+};
 
+const show404 = () => {
+  const getModal404 = document.getElementById('modal-404');
+  const modalElement = getModal404;
+  modalElement.removeAttribute('class');
+  console.log(modalElement);
+};
+
+const goBack = () => {
+  const getModal404 = document.getElementById('modal-404');
+  const modalElement = getModal404;
+  modalElement.className = 'hide-modal-404';
+  console.log(modalElement);
 };
